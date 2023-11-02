18c1b13532c8587e3520ccfd7503dadebd836469
Kwakes Prempeh <kwakesproject@gmail.com>
2023-11-02 00:15:27 +0000

    Add @ngrx/store and @ngrx/store-devtools to app providers.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient()],
+  providers: [
+    provideHttpClient(),
+    provideStore(),
+    provideStoreDevtools({
+      name: 'nominet-tech-task',
+    }),
+  ],
 };
