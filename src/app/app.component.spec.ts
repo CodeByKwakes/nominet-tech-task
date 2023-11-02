34e15720855fda97c789ac813c0d48a2038ca815
Kwakes Prempeh <kwakesproject@gmail.com>
2023-11-02 09:27:01 +0000

    Refactor imports and add API service tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,26 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome nominet-tech-task'
-    );
-  });
-
-  it(`should have as title 'nominet-tech-task'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('nominet-tech-task');
-  });
 });
--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,16 +1,62 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
   let service: ApiService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(ApiService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getAllNameservers', () => {
+    it('should return expected nameservers', () => {
+      service.getAllNameservers().subscribe((res) => {
+        expect(res.length).toBe(2);
+        expect(res).toEqual([
+          { name: 'ns1', ipAddress: '1.1.1.1' },
+          { name: 'ns2', ipAddress: '2.2.2.2' },
+        ]);
+      });
+
+      const req = httpMock.expectOne(`http://localhost:3000/nameservers`);
+      expect(req.request.method).toBe('GET');
+      req.flush([
+        { name: 'ns1', ipAddress: '1.1.1.1' },
+        { name: 'ns2', ipAddress: '2.2.2.2' },
+      ]);
+    });
+  });
+
+  describe('addNameserver', () => {
+    it('should make expected POST request', () => {
+      const nameserver = {
+        name: 'ns3',
+        ipAddress: '3.3.3.3',
+      };
+
+      service.addNameserver(nameserver).subscribe();
+
+      const req = httpMock.expectOne(`http://localhost:3000/nameservers`);
+      expect(req.request.method).toBe('POST');
+      expect(req.request.body).toEqual(nameserver);
+    });
   });
 });
